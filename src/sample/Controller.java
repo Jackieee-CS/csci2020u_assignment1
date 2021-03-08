35f43ac9645249759438d3d2782bca8169bc021d
joshu <joshuazak19@gmail.com>
2021-03-08 00:57:56 -0500

    Base UI Completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    private TextField accuracy;
+    @FXML
+    private TextField precision;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
